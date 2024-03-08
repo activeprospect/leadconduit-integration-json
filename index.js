cd0228f22b59ccdedd461e90d16a771262f5a622
Chris Grayson <chris@activeprospect.com>
2024-03-08 16:46:32 -0600

    WIP: basic working structure & start of tests

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
   outbound: {
+    authenticated_json: require('./lib/authjson'),
     json: require('./lib/json')
   },
   ui: require('./lib/ui')
---
+++ test/authjson-spec.js
@@ -0,0 +1,44 @@
+const { assert } = require('chai');
+const { requestVariables, validate } = require('../lib/authjson');
+
+describe('Authenticated JSON', function () {
+
+  describe('Request variables', function () {
+    it('makes credential_id required', function () {
+      const variables = requestVariables();
+      const credentials = variables.filter(variable => variable.name === 'credential_id');
+      assert.equal(credentials.length, 1);
+      assert.isTrue(credentials[0].required);
+    });
+  });
+
+  describe('Validate', function () {
+    let vars;
+    beforeEach(function() {
+      vars = {
+        url: 'https://example.com/deliver',
+        credential_id: 'abc123',
+        authentication_url: 'https://example.com/authenticate'
+      };
+    });
+
+    it('requires base JSON vars', function () {
+      delete vars.url;
+      assert.equal(validate(vars), 'URL is required');
+    });
+
+    it('passes validation with all required vars', function () {
+      assert.isUndefined(validate(vars));
+    });
+
+    it('fails when missing credential ID', function () {
+      delete vars.credential_id;
+      assert.equal(validate(vars), 'credential ID is required');
+    });
+
+    it('fails when missing auth URL', function () {
+      delete vars.authentication_url;
+      assert.equal(validate(vars), 'authentication URL is required');
+    });
+  });
+});
